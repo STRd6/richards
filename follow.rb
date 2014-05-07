16275021f764ab956698e1fa712e4f2b8172ba24
Daniel X Moore <yahivin@gmail.com>
2014-05-07 09:02:42 -0700

    Catching exceptions on follow

--- follow.rb
+++ follow.rb
@@ -3,5 +3,9 @@ require "./setup"
 ids = T.follower_ids("CocksDaily").take(5000)
 
 ids.sample(10).each do |id|
-  T.follow(id)
+  begin
+    T.follow(id)
+  rescue Exception => e
+    puts e.message
+  end
 end
